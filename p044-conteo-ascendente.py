41ee2914261b8f1a275ea3ab45cfacbf07dd5541
marlenejd26-codificador <marlenejd26@gmail.com>
2025-09-25 13:11:43 -0600

    Actividad 6

---
+++ p044-conteo-ascendente.py
@@ -0,0 +1,12 @@
+# p036-conteo-ascendente.py
+# Objetivo: Imprime los números de 1 a 100 usando un ciclo while
+
+print("iniciando secuencia ascendente...")
+
+c = 1
+
+while c <= 20:
+    print(f" {c}", end="")
+    c += 1
+
+print("\n ¡Secuencia completada!")
