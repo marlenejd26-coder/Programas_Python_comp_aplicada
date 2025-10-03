03b51d694dc86ac3bf0b3d06cc3d07458b9a6e2d
marlenejd26-codificador <marlenejd26@gmail.com>
2025-10-03 10:59:21 -0600

    Tarea 3

--- p063-numero-mayor.py
+++ p063-numero-mayor.py
@@ -1,4 +1,21 @@
 # p063-numero-mayor.py
-# Objetivo: 
+# Objetivo: Leer na serie de numeros introducidos y determinar cual es el mayor
+
+print("Leyendo numeros introducidos para obtener el mayor")
+
+mayor = 0
+num = 0
+
+while True:
+    num=int(input("Introduce un numero (0 para terminar): "))
+    if num == 0:
+        print ("El proceso a terminado")
+        print(f"El número mayor fue: {mayor}")
+        if input ("Deseas continuar (S/N): ").upper() != "S": break
+        break
+        
+    if num > mayor:
+        mayor=num
+
 
 
--- p064-verificar-palindromo.py
+++ p064-verificar-palindromo.py
@@ -1,2 +1,14 @@
 # p064-verificar-palindromo.py
-# Objetivo: 
+# Objetivo: Determinar si un numero es palindromo
+
+print("Determinando si un numero es palindromo")
+
+while True:
+    num = input("Introduce un número para verificar si es palindromo: ")
+
+    if num == num[::-1]:
+        print(f"El número {num} es un palíndromo.\n")
+    else:
+        print(f"El número {num} no es un palíndromo.\n")
+
+    if input ("Desea continuar (S/N) ? ").upper() != "S": break
